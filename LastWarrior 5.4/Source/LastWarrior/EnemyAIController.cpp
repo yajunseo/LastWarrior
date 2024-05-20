14cc4f392af0d77d5aaedd3af3a4b226296874d2
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2024-05-20 23:32:10 +0900

    [라워/적/야준서] 트롤 비헤이비어 트리, 블랙 보드 적용

--- LastWarrior 5.4/Source/LastWarrior/EnemyAIController.cpp
+++ LastWarrior 5.4/Source/LastWarrior/EnemyAIController.cpp
@@ -3,3 +3,20 @@
 
 #include "EnemyAIController.h"
 
+#include "BehaviorTree/BlackboardComponent.h"
+
+AEnemyAIController::AEnemyAIController()
+{
+	BBComponent = CreateDefaultSubobject<UBlackboardComponent>(TEXT("BlackBoard Component"));
+}
+
+void AEnemyAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(BT != nullptr && BB != nullptr)
+	{
+		RunBehaviorTree(BT);
+		UseBlackboard(BB, BBComponent);
+	}
+}
--- LastWarrior 5.4/Source/LastWarrior/EnemyAIController.h
+++ LastWarrior 5.4/Source/LastWarrior/EnemyAIController.h
@@ -14,4 +14,18 @@ class LASTWARRIOR_API AEnemyAIController : public AAIController
 {
 	GENERATED_BODY()
 	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = AI, meta = (AllowPrivateAccess = "true"))
+	class UBehaviorTree* BT;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = AI, meta = (AllowPrivateAccess = "true"))
+	class UBlackboardData* BB;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = AI, meta = (AllowPrivateAccess = "true"))
+	class UBlackboardComponent* BBComponent;
+
+public:
+	AEnemyAIController();
+	
+protected:
+	virtual void BeginPlay() override;
 };
